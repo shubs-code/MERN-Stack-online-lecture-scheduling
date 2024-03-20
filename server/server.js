4da98e1aa231ad21b7783db4aff13717fa0bc5e1
shubs-code <shrubexer@gmail.con>
2024-03-20 10:16:17 +0530

    cors all origin allowed

--- server/server.js
+++ server/server.js
@@ -4,11 +4,8 @@ require('dotenv').config()
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(express.json());
